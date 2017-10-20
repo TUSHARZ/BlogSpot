a757c962d30311d952b048e72b993420e294f09f
Rohan Taneja <rohan@Rohan-Taneja-553-MPR00070.local>
2017-10-21 02:50:25 +0530

    improved bloglist.xml layout

--- app/src/main/java/com/example/tushar/menu/Blog.java
+++ app/src/main/java/com/example/tushar/menu/Blog.java
@@ -4,21 +4,23 @@ package com.example.tushar.menu;
  * Created by TUSHAR on 18-03-2017.
  */
 
- class Blog {
+class Blog {
     private String Title;
     private String Description;
     private String Image;
     private String Profile;
     private String name;
-  public Blog(){
 
-  }
-    public Blog(String Title, String Description, String Image,String Profile,String name) {
-       this.Title = Title;
-       this.Description = Description;
+    public Blog() {
+
+    }
+
+    public Blog(String Title, String Description, String Image, String Profile, String name) {
+        this.Title = Title;
+        this.Description = Description;
         this.Image = Image;
-        this.Profile=Profile;
-        this.name=name;
+        this.Profile = Profile;
+        this.name = name;
     }
 
     public String getTitle() {
@@ -44,16 +46,18 @@ package com.example.tushar.menu;
     public void setImage(String image) {
         this.Image = image;
     }
-   public void setProfile(String Profile){
-       this.Profile=Profile;
-   }
-    public String getProfile(){
+
+    public void setProfile(String Profile) {
+        this.Profile = Profile;
+    }
+
+    public String getProfile() {
         return Profile;
     }
 
 
     public String getName() {
-        return "Posted by :" + name;
+        return "Posted by: " + name;
     }
 
     public void setName(String name) {
